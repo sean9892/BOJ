2ea37ea000f246289abefa2fd32b68ee6b4c2da6
sean9892 <ensyforxbox@gmail.com>
2019-12-24 19:22:16 +0900

    BOJ 10815
    간단하게 배열 arr[x]에는 x가 있는지 저장하면 된다. 음수 인덱스는 쓸수 없다고 말할 수 있지만, 적당히 평행이동시키면 되지 않겠는가.

---
+++ 10815.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int v=10000000;
+int arr[20000010];
+
+int main(void){
+	ios::sync_with_stdio(0);cin.tie(0);
+	int n,m;
+	cin>>n;
+	while(n--){
+		int x;
+		cin>>x;
+		arr[x+v]=1;
+	}
+	cin>>m;
+	while(m--){
+		int x;
+		cin>>x;
+		cout<<arr[x+v]<<" ";
+	}
+}
